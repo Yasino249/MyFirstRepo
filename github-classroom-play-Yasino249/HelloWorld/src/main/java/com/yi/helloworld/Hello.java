cd2e7593c9ed5e03c4fc3b7fb03e3ca1c4bc647f
Yassin Ibrahim <its.yi@icloud.com>
2021-05-23 20:03:27 -0400

    Hello World in NetBeans

---
+++ github-classroom-play-Yasino249/HelloWorld/src/main/java/com/yi/helloworld/Hello.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author YassinIbrahim
+ * its.yi@icloud.com
+ * 5/23/2021
+ * My first Hello, World! program in an IDE
+ */
+package com.yi.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Yassin!");
+    }
+}
